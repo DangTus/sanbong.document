99f719b2f7c67d3370fa41edb1301d26c1495aec
dangtus <dangtu9520@gmail.com>
2023-05-30 16:25:25 +0700

    verification owner account

--- verification.js
+++ verification.js
@@ -8,3 +8,44 @@ function getOwner() {
         error: console.log,
     });
 }
+
+function approve() {
+    $.ajax({
+        url: `${url_api}/admin/verification`,
+        type: "post",
+        data: {
+            id: 3,
+            status_id: 2,
+        },
+        success(res) {
+            console.log(res);
+        },
+        error: console.log,
+    });
+}
+
+function refuse() {
+    $.ajax({
+        url: `${url_api}/admin/verification`,
+        type: "post",
+        data: {
+            id: 3,
+            status_id: 3,
+        },
+        success(res) {
+            console.log(res);
+        },
+        error: console.log,
+    });
+}
+
+function verificationFail() {
+    $.ajax({
+        url: `${url_api}/admin/verification`,
+        type: "post",
+        success(res) {
+            console.log(res);
+        },
+        error: console.log,
+    });
+}
