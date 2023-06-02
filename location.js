ab7dd89ac488f3cbfe687baff50cb75ca12cb706
dangtus <dangtu9520@gmail.com>
2023-06-02 11:54:08 +0700

    owner / update info location

--- location.js
+++ location.js
@@ -66,3 +66,55 @@ function getTimeSlot() {
         error: console.log,
     });
 }
+
+function booking() {
+    $.ajax({
+        url: `${url_api}/location/book-field`,
+        type: "post",
+        data: {
+            timeslot_id: 3,
+            field_id: 1,
+            customer_id: 5,
+            customer_name: "Nguyễn Thành Viên",
+            phone_number: "0123456789",
+            date_book: "2023-06-10",
+            price: 200000,
+        },
+        success(res) {
+            console.log(res);
+        },
+        error: console.log,
+    });
+}
+
+function uploadImage() {
+    const listImageFileElement = document.querySelector("#images");
+
+    var formData = new FormData();
+    formData.append("location_id", 1);
+    formData.append("name", "Hòa Tiến Vip");
+    formData.append("description", "Sân này vip nhất Hòa Tiến");
+    formData.append("time_open", "07:00");
+    formData.append("time_close", "22:00");
+    formData.append("ward_id", 20227);
+    formData.append("address", "Dương Sơn");
+    formData.append("link_map", "https://goo.gl/maps/UisAzZN1mEVQC8K19");
+    formData.append("status_id", 1);
+
+    for (let imageFileElement of listImageFileElement.files) {
+        formData.append("images[]", imageFileElement);
+    }
+
+    $.ajax({
+        url: `${url_api}/owner/location/update`,
+        type: "post",
+        data: formData,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success(res) {
+            console.log(res);
+        },
+        error: console.log,
+    });
+}
